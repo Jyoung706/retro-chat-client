07cacceccac3bd433a3fec767e59278b991c5b13
jYoung706 <wnsdud9476@gmail.com>
2025-01-02 22:48:59 +0900

    [Add] chatRoomlist dummy data

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,3 +1,21 @@
-export default function Main() {
-  return <div>this is main page</div>;
+"use client";
+import { useEffect } from "react";
+import axios from "../../lib/axios";
+
+export default function MainPage() {
+  const getChatRoomList = async () => {
+    const chatRoomList = await axios.get("/test/chatRoomList.json");
+    console.log("chat room List : ", chatRoomList);
+  };
+
+  useEffect(() => {
+    getChatRoomList();
+  }, []);
+
+  return (
+    <div className='main-page'>
+      <h1>메인 페이지</h1>
+      {/* 페이지 컨텐츠 */}
+    </div>
+  );
 }
